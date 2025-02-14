8fb23c91da1ee645bbde975513261fdcd095ce32
Marius Storhaug <marstor@hotmail.com>
2025-02-14 17:28:33 +0100

    Update configuration paths and streamline container scripts for improved clarity

--- tests/Advanced/Planets/Planets.Container.ps1
+++ tests/Advanced/Planets/Planets.Container.ps1
@@ -1,7 +1,7 @@
 ﻿@{
-    Path = Get-ChildItem -Path $PSScriptRoot -Filter *.Tests.ps1
+    Path = Get-ChildItem -Path $PSScriptRoot -Filter *.Tests.ps1 | Select-Object -ExpandProperty FullName
     Data = @{
-        Path    = Get-ChildItem -Path $PSScriptRoot -Filter *.Data.ps1
+        Path    = Get-ChildItem -Path $PSScriptRoot -Filter *.Data.ps1 | Select-Object -ExpandProperty FullName
         Debug   = $false
         Verbose = $false
     }
