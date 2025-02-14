47454d570dacce182f976496896ce3e558e7df50
Marius Storhaug <marstor@hotmail.com>
2025-02-14 17:20:42 +0100

    Reduce JSON depth in container logging for improved readability

--- scripts/main.ps1
+++ scripts/main.ps1
@@ -237,7 +237,7 @@ LogGroup 'Load configuration - Result' {
 
 LogGroup 'Load containers' {
     $containers = Get-PesterContainer -Path $configuration.Run.Path
-    Write-Host ($containers | ConvertTo-Json -Depth 2 -WarningAction SilentlyContinue)
+    Write-Host ($containers | ConvertTo-Json -Depth 1 -WarningAction SilentlyContinue)
 }
 
 $configuration.Run.Container = $containers
