4038037fd20ce26abf56cf467c90c2be30ce2787
Marius Storhaug <marstor@hotmail.com>
2025-02-14 19:41:59 +0100

    Remove redundant tests for animal count and names in Animals.Tests.ps1

--- tests/Advanced/Animals/Animals.Tests.ps1
+++ tests/Advanced/Animals/Animals.Tests.ps1
@@ -24,20 +24,3 @@ Describe 'Animals Module Tests' {
         }
     }
 }
-
-Describe 'Animals Module Tests' {
-    Context 'Animal Count' {
-        It 'Should return the expected number of animals' {
-            $expectedCount = $AnimalsSettings.AnimalCount
-            $actualCount = $AnimalsSettings.AnimalNames.Count
-            $actualCount | Should -Be $expectedCount
-        }
-    }
-    Context 'Animal Names' {
-        It 'Should return the expected list of animal names' {
-            $expectedNames = $AnimalsSettings.AnimalNames
-            $actualNames = $AnimalsSettings.AnimalNames
-            $actualNames | Should -BeExactly $expectedNames
-        }
-    }
-}
