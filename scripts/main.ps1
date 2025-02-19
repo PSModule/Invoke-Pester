589ca1838c369e908a30b57ec4573c4eddc5b4ec
Marius Storhaug <marstor@hotmail.com>
2025-02-19 15:34:51 +0100

    🩹 [Patch]: Fix typo in output message for container search in Run.Path directories

--- scripts/main.ps1
+++ scripts/main.ps1
@@ -222,7 +222,7 @@ LogGroup 'Load configuration - Add containers' {
     # Load configuration - Add containers
     if ($configuration.Run.Container.Count -eq 0) {
         # If no containers are specified, search for "*.Container.*" files in each Run.Path directory
-        Write-Ouptut "No containers specified. Searching for containers in Run.Path directories."
+        Write-Output "No containers specified. Searching for containers in Run.Path directories."
         foreach ($testDir in $configuration.Run.Path) {
             Write-Output "Processing directory [$testDir]"
             if (Test-Path -LiteralPath $testDir -PathType Container) {
