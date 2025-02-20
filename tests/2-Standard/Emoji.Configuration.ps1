7af52a59566cc73137126e2668caec60bdbb2179
Marius Storhaug <marstor@hotmail.com>
2025-02-20 11:59:52 +0100

    Add container loading to Emoji.Configuration for enhanced test execution

--- tests/2-Standard/Emoji.Configuration.ps1
+++ tests/2-Standard/Emoji.Configuration.ps1
@@ -2,6 +2,8 @@
     Run        = @{
         Path     = $PSScriptRoot
         PassThru = $true
+        Container = Get-ChildItem -Path $PSScriptRoot -Filter *.Container.* -Recurse |
+            ForEach-Object { . $_ } | ForEach-Object { New-PesterContainer @_ }
     }
     TestResult = @{
         Enabled       = $true
