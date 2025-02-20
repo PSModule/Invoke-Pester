d21fd79321677d83d7ece4a5ebf775da58a15966
Marius Storhaug <marstor@hotmail.com>
2025-02-20 12:07:33 +0100

    Remove container loading from Emoji.Configuration to streamline test execution

--- tests/2-Standard/Emoji.Configuration.ps1
+++ tests/2-Standard/Emoji.Configuration.ps1
@@ -2,8 +2,6 @@
     Run        = @{
         Path     = $PSScriptRoot
         PassThru = $true
-        Container = Get-ChildItem -Path $PSScriptRoot -Filter *.Container.* -Recurse |
-            ForEach-Object { . $_ } | ForEach-Object { New-PesterContainer @_ }
     }
     TestResult = @{
         Enabled       = $true
