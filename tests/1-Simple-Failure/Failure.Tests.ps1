d74461a553f00a76aebeaff556097c2f9e5b559c
Marius Storhaug <marstor@hotmail.com>
2025-02-22 12:24:04 +0100

    🩹 [Patch]: Add simple failure test case and update workflow to include continue-on-error option

---
+++ tests/1-Simple-Failure/Failure.Tests.ps1
@@ -0,0 +1,16 @@
+﻿[Diagnostics.CodeAnalysis.SuppressMessageAttribute(
+    'PSReviewUnusedParameter', '',
+    Justification = 'Required for Pester tests'
+)]
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute(
+    'PSUseDeclaredVarsMoreThanAssignments', '',
+    Justification = 'Required for Pester tests'
+)]
+[CmdletBinding()]
+param()
+
+Describe 'Failure' {
+    It 'True should be false' {
+        $true | Should -Be $false
+    }
+}
