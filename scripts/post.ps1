2c3263cb4bbb0ee56e9b855b4207e9bd54651e98
Marius Storhaug <marstor@hotmail.com>
2025-02-26 12:48:52 +0100

    🩹 [Patch]: Refactor Pester invocation steps and add post-processing script

---
+++ scripts/post.ps1
@@ -0,0 +1,127 @@
+﻿[CmdletBinding()]
+param()
+
+LogGroup 'Setup prerequisites' {
+    'Pester', 'Hashtable', 'TimeSpan', 'Markdown' | ForEach-Object {
+        Install-PSResource -Name $_ -Verbose:$false -WarningAction SilentlyContinue -TrustRepository -Repository PSGallery
+        Import-Module -Name $_ -Verbose:$false
+    }
+    Import-Module "$PSScriptRoot/Helpers.psm1"
+}
+
+LogGroup 'Get test kit versions' {
+    $pesterModule = Get-PSResource -Name Pester -Verbose:$false | Sort-Object Version -Descending | Select-Object -First 1
+
+    [PSCustomObject]@{
+        PowerShell = $PSVersionTable.PSVersion.ToString()
+        Pester     = $pesterModule.Version
+    } | Format-List
+}
+
+LogGroup 'Load inputs' {
+    $providedItem = Resolve-Path -Path $env:GITHUB_ACTION_INPUT_Path | Select-Object -ExpandProperty Path | Get-Item
+    if ($providedItem -is [System.IO.DirectoryInfo]) {
+        $providedPath = $providedItem.FullName
+    } elseif ($providedItem -is [System.IO.FileInfo]) {
+        $providedPath = $providedItem.Directory.FullName
+    } else {
+        Write-GitHubError "❌ Provided path [$providedItem] is not a valid directory or file."
+        exit 1
+    }
+
+    $inputs = @{
+        Path                               = $providedPath
+
+        Run_Path                           = $env:GITHUB_ACTION_INPUT_Run_Path
+        Run_ExcludePath                    = $env:GITHUB_ACTION_INPUT_Run_ExcludePath
+        Run_ScriptBlock                    = $env:GITHUB_ACTION_INPUT_Run_ScriptBlock
+        Run_Container                      = $env:GITHUB_ACTION_INPUT_Run_Container
+        Run_TestExtension                  = $env:GITHUB_ACTION_INPUT_Run_TestExtension
+        Run_Exit                           = $env:GITHUB_ACTION_INPUT_Run_Exit
+        Run_Throw                          = $env:GITHUB_ACTION_INPUT_Run_Throw
+        Run_SkipRun                        = $env:GITHUB_ACTION_INPUT_Run_SkipRun
+        Run_SkipRemainingOnFailure         = $env:GITHUB_ACTION_INPUT_Run_SkipRemainingOnFailure
+
+        Filter_Tag                         = $env:GITHUB_ACTION_INPUT_Filter_Tag
+        Filter_ExcludeTag                  = $env:GITHUB_ACTION_INPUT_Filter_ExcludeTag
+        Filter_Line                        = $env:GITHUB_ACTION_INPUT_Filter_Line
+        Filter_ExcludeLine                 = $env:GITHUB_ACTION_INPUT_Filter_ExcludeLine
+        Filter_FullName                    = $env:GITHUB_ACTION_INPUT_Filter_FullName
+
+        CodeCoverage_Enabled               = $env:GITHUB_ACTION_INPUT_CodeCoverage_Enabled
+        CodeCoverage_OutputFormat          = $env:GITHUB_ACTION_INPUT_CodeCoverage_OutputFormat
+        CodeCoverage_OutputPath            = $env:GITHUB_ACTION_INPUT_CodeCoverage_OutputPath
+        CodeCoverage_OutputEncoding        = $env:GITHUB_ACTION_INPUT_CodeCoverage_OutputEncoding
+        CodeCoverage_Path                  = $env:GITHUB_ACTION_INPUT_CodeCoverage_Path
+        CodeCoverage_ExcludeTests          = $env:GITHUB_ACTION_INPUT_CodeCoverage_ExcludeTests
+        CodeCoverage_RecursePaths          = $env:GITHUB_ACTION_INPUT_CodeCoverage_RecursePaths
+        CodeCoverage_CoveragePercentTarget = $env:GITHUB_ACTION_INPUT_CodeCoverage_CoveragePercentTarget
+        CodeCoverage_UseBreakpoints        = $env:GITHUB_ACTION_INPUT_CodeCoverage_UseBreakpoints
+        CodeCoverage_SingleHitBreakpoints  = $env:GITHUB_ACTION_INPUT_CodeCoverage_SingleHitBreakpoints
+
+        TestResult_Enabled                 = $env:GITHUB_ACTION_INPUT_TestResult_Enabled
+        TestResult_OutputFormat            = $env:GITHUB_ACTION_INPUT_TestResult_OutputFormat
+        TestResult_OutputPath              = $env:GITHUB_ACTION_INPUT_TestResult_OutputPath
+        TestResult_OutputEncoding          = $env:GITHUB_ACTION_INPUT_TestResult_OutputEncoding
+        TestResult_TestSuiteName           = $env:GITHUB_ACTION_INPUT_TestResult_TestSuiteName
+
+        Should_ErrorAction                 = $env:GITHUB_ACTION_INPUT_Should_ErrorAction
+
+        Debug_ShowFullErrors               = $env:GITHUB_ACTION_INPUT_Debug_ShowFullErrors
+        Debug_WriteDebugMessages           = $env:GITHUB_ACTION_INPUT_Debug_WriteDebugMessages
+        Debug_WriteDebugMessagesFrom       = $env:GITHUB_ACTION_INPUT_Debug_WriteDebugMessagesFrom
+        Debug_ShowNavigationMarkers        = $env:GITHUB_ACTION_INPUT_Debug_ShowNavigationMarkers
+        Debug_ReturnRawResultObject        = $env:GITHUB_ACTION_INPUT_Debug_ReturnRawResultObject
+
+        Output_Verbosity                   = $env:GITHUB_ACTION_INPUT_Output_Verbosity
+        Output_StackTraceVerbosity         = $env:GITHUB_ACTION_INPUT_Output_StackTraceVerbosity
+        Output_CIFormat                    = $env:GITHUB_ACTION_INPUT_Output_CIFormat
+        Output_CILogLevel                  = $env:GITHUB_ACTION_INPUT_Output_CILogLevel
+        Output_RenderMode                  = $env:GITHUB_ACTION_INPUT_Output_RenderMode
+
+        TestDrive_Enabled                  = $env:GITHUB_ACTION_INPUT_TestDrive_Enabled
+        TestRegistry_Enabled               = $env:GITHUB_ACTION_INPUT_TestRegistry_Enabled
+    }
+
+    [pscustomobject]($inputs.GetEnumerator() | Where-Object { -not [string]::IsNullOrEmpty($_.Value) }) | Format-List
+}
+
+$testResults = Invoke-Pester -Configuration $configuration
+
+LogGroup 'Test results' {
+    if ($null -eq $testResults) {
+        Write-GitHubError '❌ No test results were returned.'
+        exit 1
+    }
+
+    $testResults | Format-List
+}
+
+LogGroup 'Test results summary' {
+    Set-GitHubStepSummary -Summary ($testResults | Set-PesterReportSummary)
+}
+
+LogGroup 'Set outputs' {
+    Set-GitHubOutput -Name 'TestSuiteName' -Value $testResults.Configuration.TestResult.TestSuiteName.Value
+    Set-GitHubOutput -Name 'TestResultEnabled' -Value $testResults.Configuration.TestResult.Enabled.Value
+    Set-GitHubOutput -Name 'TestResultOutputPath' -Value $testResults.Configuration.TestResult.OutputPath.Value
+    Set-GitHubOutput -Name 'CodeCoverageEnabled' -Value $testResults.Configuration.CodeCoverage.Enabled.Value
+    Set-GitHubOutput -Name 'CodeCoverageOutputPath' -Value $testResults.Configuration.CodeCoverage.OutputPath.Value
+}
+
+LogGroup 'Exit' {
+    if ($testResults.Result -eq 'Passed') {
+        Write-GitHubNotice '✅ All tests passed.'
+        $script:exit = 0
+    } else {
+        if ($failedTests -gt 0) {
+            Write-GitHubError "❌ Some [$failedTests] tests failed."
+            $script:exit = $failedTests
+        } else {
+            Write-GitHubError '❌ Some tests failed.'
+            $script:exit = 1
+        }
+    }
+}
+
+exit $script:exit
