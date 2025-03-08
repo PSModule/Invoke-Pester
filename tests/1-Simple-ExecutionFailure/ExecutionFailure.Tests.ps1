b9a84ec0215cb5ab73f67409944b7b885fcb9907
Marius Storhaug <marstor@hotmail.com>
2025-03-08 12:27:39 +0100

    🩹 [Patch]: Add new test job for simple execution failure in Action-Test.yml and create corresponding test script

---
+++ tests/1-Simple-ExecutionFailure/ExecutionFailure.Tests.ps1
@@ -0,0 +1,15 @@
+﻿[Diagnostics.CodeAnalysis.SuppressMessageAttribute(
+    'PSReviewUnusedParameter', '',
+    Justification = 'Required for Pester tests'
+)]
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute(
+    'PSUseDeclaredVarsMoreThanAssignments', '',
+    Justification = 'Required for Pester tests'
+)]
+[CmdletBinding()]
+param(
+    [Parameter(Mandatory)]
+    [string] $Path
+)
+
+Describe 'Failure' {}
