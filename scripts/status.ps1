e11df9ce4a792909c7352f84d62dbec47f3bac9a
Marius Storhaug <marstor@hotmail.com>
2025-03-12 11:16:31 +0100

    🩹 [Patch]: Enhance status reporting (#40)
    ## Description
    
    This pull request includes updates to the `action.yml` and
    `scripts/status.ps1` files to enhance the reporting of test results. The
    most important changes are the addition of new environment variables and
    the corresponding updates to the script to handle these new variables.
    
    Enhancements to test result reporting:
    
    *
    [`action.yml`](diffhunk://#diff-1243c5424efaaa19bd8e813c5e6f6da46316e63761421b3e5f5c8ced9a36e6b6L373-R384):
    Added several new environment variables to capture detailed test result
    metrics, including `Result`, `FailedCount`, `FailedBlocksCount`,
    `FailedContainersCount`, `PassedCount`, `SkippedCount`,
    `InconclusiveCount`, `NotRunCount`, and `TotalCount`.
    *
    [`scripts/status.ps1`](diffhunk://#diff-9c125f16c0d30d308a7ddca8f93f2dbb493adcd6364d6f89db8b410a5285f381L2-R32):
    Updated the script to handle the new environment variables and log their
    values, providing a more comprehensive summary of the test results.
    
    ## Type of change
    
    <!-- Use the check-boxes [x] on the options that are relevant. -->
    
    - [ ] 📖 [Docs]
    - [ ] 🪲 [Fix]
    - [x] 🩹 [Patch]
    - [ ] ⚠️ [Security fix]
    - [ ] 🚀 [Feature]
    - [ ] 🌟 [Breaking change]
    
    ## Checklist
    
    <!-- Use the check-boxes [x] on the options that are relevant. -->
    
    - [x] I have performed a self-review of my own code
    - [x] I have commented my code, particularly in hard-to-understand areas

--- scripts/status.ps1
+++ scripts/status.ps1
@@ -1,16 +1,35 @@
 ﻿$PSStyle.OutputRendering = 'Ansi'
-$executed = $env:PSMODULE_INVOKE_PESTER_INTERNAL_Executed -eq 'true'
 $outcome = $env:PSMODULE_INVOKE_PESTER_INTERNAL_Outcome
 $conclusion = $env:PSMODULE_INVOKE_PESTER_INTERNAL_Conclusion
+$executed = $env:PSMODULE_INVOKE_PESTER_INTERNAL_Executed -eq 'true'
+$result = $env:PSMODULE_INVOKE_PESTER_INTERNAL_Result
+$failedCount = $env:PSMODULE_INVOKE_PESTER_INTERNAL_FailedCount
+$failedBlocksCount = $env:PSMODULE_INVOKE_PESTER_INTERNAL_FailedBlocksCount
+$failedContainersCount = $env:PSMODULE_INVOKE_PESTER_INTERNAL_FailedContainersCount
+$passedCount = $env:PSMODULE_INVOKE_PESTER_INTERNAL_PassedCount
+$skippedCount = $env:PSMODULE_INVOKE_PESTER_INTERNAL_SkippedCount
+$inconclusiveCount = $env:PSMODULE_INVOKE_PESTER_INTERNAL_InconclusiveCount
+$notRunCount = $env:PSMODULE_INVOKE_PESTER_INTERNAL_NotRunCount
+$totalCount = $env:PSMODULE_INVOKE_PESTER_INTERNAL_TotalCount
 
 "Executed=$executed" >> $env:GITHUB_OUTPUT
 
-[PSCustomObject]@{
-    Executed   = $executed
-    Outcome    = $outcome
-    Conclusion = $conclusion
-} | Format-List | Out-String
-
+LogGroup 'Outputs' {
+    [PSCustomObject]@{
+        Outcome               = $outcome
+        Conclusion            = $conclusion
+        Executed              = $executed
+        Result                = $result
+        FailedCount           = $failedCount
+        FailedBlocksCount     = $failedBlocksCount
+        FailedContainersCount = $failedContainersCount
+        PassedCount           = $passedCount
+        SkippedCount          = $skippedCount
+        InconclusiveCount     = $inconclusiveCount
+        NotRunCount           = $notRunCount
+        TotalCount            = $totalCount
+    } | Format-List | Out-String
+}
 # If the tests did not execute, exit with a failure code
 if ($executed -ne 'true') {
     Write-Error 'Tests did not execute.'
