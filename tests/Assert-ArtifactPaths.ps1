01ddc58f3bd1200955354c946fd7f1d5f3e8161f
Marius Storhaug <marstor@hotmail.com>
2026-09-10 14:23:13 +0200

    Require a separate report output directory

--- tests/Assert-ArtifactPaths.ps1
+++ tests/Assert-ArtifactPaths.ps1
@@ -10,11 +10,11 @@ $outputDirectory = switch ($Layout) {
         Join-Path -Path $env:GITHUB_WORKSPACE -ChildPath 'tests/2-Standard'
     }
     'PSModule' {
-        Join-Path -Path $env:GITHUB_WORKSPACE -ChildPath '.PSModule'
+        Join-Path -Path $env:GITHUB_WORKSPACE -ChildPath '.PSModule/outputs'
     }
 }
 
-$temporaryDirectory = $outputDirectory
+$temporaryDirectory = Join-Path -Path $env:GITHUB_WORKSPACE -ChildPath 'tests/2-Standard'
 
 $expectedPaths = @{
     '.temp configuration'       = Join-Path -Path $temporaryDirectory -ChildPath '.temp/Invoke-Pester.Configuration.ps1'
@@ -33,7 +33,6 @@ foreach ($artifact in $expectedPaths.GetEnumerator()) {
 if ($Layout -eq 'PSModule') {
     $workingDirectory = Join-Path -Path $env:GITHUB_WORKSPACE -ChildPath 'tests/2-Standard'
     $legacyPaths = @(
-        (Join-Path -Path $workingDirectory -ChildPath '.temp'),
         (Join-Path -Path $workingDirectory -ChildPath 'TestResult'),
         (Join-Path -Path $workingDirectory -ChildPath 'CodeCoverage')
     )
